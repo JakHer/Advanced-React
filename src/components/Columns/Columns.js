466a46750694566c3f8de628737c7c8f2ad7fa2f
Kuba <kubahermyt@gmail.com>
2020-08-26 21:07:10 +0200

    feat: HOC added instead of hook

--- src/components/Columns/Columns.js
+++ src/components/Columns/Columns.js
@@ -1,49 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
 import cx from "classnames";
 import styles from "./Columns.module.scss";
+import withCollapse from "../../hoc/withCollapse";
 
-const Columns = () => {
-  const [collapsed, isCollapsed] = useState(false);
+const Columns = ({ isCollapsed, toggle }) => (
+  <div className={(cx("columns"), styles.Columns)}>
+    <button onClick={toggle} className="button">
+      {isCollapsed ? "show" : "hide"}
+    </button>
 
-  return (
-    <div className={(cx("columns"), styles.Columns)}>
-      {collapsed && (
-        <button onClick={() => isCollapsed(false)} className="button">
-          Show
-        </button>
-      )}
+    {!isCollapsed && (
+      <>
+        <div className={cx("column", styles.Column)}>
+          <div className="notification is-primary">
+            <p className={cx("subtitle is-3 is-center", styles.SubTitle)}>Siema</p>
+          </div>
+        </div>
+        <div className={cx("column", styles.Column)}>
+          <div className="notification is-primary">
+            <p className={cx("subtitle is-3 is-center", styles.SubTitle)}>Siema</p>
+          </div>
+        </div>
+        <div className={cx("column", styles.Column)}>
+          <div className="notification is-primary">
+            <p className={cx("subtitle is-3 is-center", styles.SubTitle)}>Siema</p>
+          </div>
+        </div>
+        <div className={cx("column", styles.Column)}>
+          <div className="notification is-primary">
+            <p className={cx("subtitle is-3 is-center", styles.SubTitle)}>Siema</p>
+          </div>
+        </div>
+      </>
+    )}
+  </div>
+);
 
-      {!collapsed && (
-        <button onClick={() => isCollapsed(true)} className="button">
-          Hide
-        </button>
-      )}
-      {!collapsed && (
-        <>
-          <div className={cx("column", styles.Column)}>
-            <div className="notification is-primary">
-              <p className={cx("subtitle is-3 is-center", styles.SubTitle)}>Siema</p>
-            </div>
-          </div>
-          <div className={cx("column", styles.Column)}>
-            <div className="notification is-primary">
-              <p className={cx("subtitle is-3 is-center", styles.SubTitle)}>Siema</p>
-            </div>
-          </div>
-          <div className={cx("column", styles.Column)}>
-            <div className="notification is-primary">
-              <p className={cx("subtitle is-3 is-center", styles.SubTitle)}>Siema</p>
-            </div>
-          </div>
-          <div className={cx("column", styles.Column)}>
-            <div className="notification is-primary">
-              <p className={cx("subtitle is-3 is-center", styles.SubTitle)}>Siema</p>
-            </div>
-          </div>
-        </>
-      )}
-    </div>
-  );
-};
-
-export default Columns;
+export default withCollapse(Columns);

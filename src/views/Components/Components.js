abaa7c4d58f21387feab0a3ee330d13b67bb51d8
Kuba <kubahermyt@gmail.com>
2020-09-01 11:03:12 +0200

    feat: useRecducer and task searching

--- src/views/Components/Components.js
+++ src/views/Components/Components.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import styles from "./Components.module.scss";
 import cx from "classnames";
 
 const Components = () => {
-  const [inputContent, setInputContent] = useState("Write task to do");
+  const [inputContent, setInputContent] = useReducer((state, newState) => ({ ...state, ...newState }), {
+    searchInputContent: "",
+    itemInputContent: "",
+  });
   const [itemsList, setItemsList] = useState([
     {
       id: "1",
@@ -12,12 +15,15 @@ const Components = () => {
   ]);
 
   const handleInputContent = (e) => {
-    setInputContent(e.target.value);
+    console.log(e.target.value);
+    setInputContent({
+      [e.target.name]: e.target.value,
+    });
   };
 
   const addNewItem = () => {
     const newElement = {
-      content: inputContent,
+      content: inputContent.itemInputContent,
       id: itemsList.length + 1,
     };
     setItemsList([...itemsList, newElement]);
@@ -30,17 +36,38 @@ const Components = () => {
 
   return (
     <div className={cx("box mt-4", styles.Container)}>
-      <input placeholder={inputContent} className="input mb-4" onChange={handleInputContent} />
+      <input
+        name="itemInputContent"
+        placeholder="Add new task"
+        value={inputContent.itemInputContent}
+        className="input mb-4"
+        onChange={handleInputContent}
+        type="text"
+      />
       <button className="button is-success mb-4" onClick={addNewItem}>
         Add new item
       </button>
 
-      {itemsList.map((item) => (
-        <div key={item.id} className={cx("notification is-info", styles.Notification)}>
-          <button onClick={() => removeItem(item.id)} className="delete"></button>
-          {item.content}
-        </div>
-      ))}
+      <label className="label" htmlFor="searchTask" name="searchInputContent">
+        Search for task
+      </label>
+      <input
+        type="text"
+        name="searchInputContent"
+        className="input mb-4"
+        placeholder="Search for task"
+        value={inputContent.searchInputContent}
+        onChange={handleInputContent}
+      />
+
+      {itemsList
+        .filter((item) => item.content.toLocaleLowerCase().includes(inputContent.searchInputContent.toLowerCase()))
+        .map((item) => (
+          <div key={item.id} className={cx("notification is-info", styles.Notification)}>
+            <button onClick={() => removeItem(item.id)} className="delete"></button>
+            {item.content}
+          </div>
+        ))}
     </div>
   );
 };

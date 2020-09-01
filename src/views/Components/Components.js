88e4af32bbc408e8927e0f5d12d1f11dde56270a
Kuba <kubahermyt@gmail.com>
2020-09-01 09:44:29 +0200

    feat: hooks playground

--- src/views/Components/Components.js
+++ src/views/Components/Components.js
@@ -4,6 +4,7 @@ import cx from "classnames";
 
 const Components = () => {
   const [count, setCount] = useState(0);
+  const [discount, setDiscount] = useState(0);
   const [input, setInput] = useState("Kuba");
 
   const inputValueHandler = (e) => {
@@ -14,13 +15,40 @@ const Components = () => {
     <>
       <div className={cx("box mt-4", styles.Container)}>
         <div className={styles.Wrapper}>
-          <button className={cx("button is-success")} onClick={() => setCount(count + 1)}>
+          <button className={cx("button is-success")} onClick={() => setCount(count < 3 ? count + 1 : count)}>
             +1
           </button>
           <h2 className="is-title-3">Counter: {count}</h2>
-          <button className={cx("button is-danger")} onClick={() => setCount(count - 1)}>
+          <button className={cx("button is-danger")} onClick={() => setCount(count > 0 ? count - 1 : 0)}>
             -1
           </button>
+        </div>
+      </div>
+
+      <div className={cx("box mt-4", styles.Container)}>
+        <div className={styles.Wrapper}>
+          <button className={cx("button is-success")} onClick={() => setDiscount(discount < 3 ? discount + 1 : discount)}>
+            +1
+          </button>
+          <h2 className="is-title-3">Discount: {discount}</h2>
+          <button className={cx("button is-danger")} onClick={() => setDiscount(discount > 0 ? discount - 1 : 0)}>
+            -1
+          </button>
+        </div>
+      </div>
+
+      <div className={cx("container mt-4", styles.Container)}>
+        <div className="box">
+          <div className="box">
+            <p>Normal: {count}</p>
+            <p>Price: {count * 3}$</p>
+          </div>
+          <div className="box">
+            <p>Discount: {discount}</p>
+            <p>Price: {discount * 1.5}$</p>
+          </div>
+          <p>Total: {discount * 1.5 + count * 3}$</p>
+          <button className="button is-success">Buy now!</button>
         </div>
       </div>
 

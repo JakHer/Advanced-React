fb80c697784e34342ddd2fb06f10df0e02bd9e5a
Kuba <kubahermyt@gmail.com>
2020-09-08 12:48:46 +0200

    feat : state sharing

--- src/views/Components/MultiStep/MultiStep.js
+++ src/views/Components/MultiStep/MultiStep.js
@@ -1,25 +1,37 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import cx from "classnames";
 import styles from "./MultiStep.module.scss";
 
 const Page = ({ children, pageIndex }) => {
-  const { changePage, currentPage } = useContext(WizzardContext);
+  const { currentPage, updatePageIndexes } = useContext(WizzardContext);
 
-  return currentPage === pageIndex ? <>{children}</> : null;
+  useEffect(() => {
+    updatePageIndexes(pageIndex);
+  });
+
+  return currentPage === pageIndex ? children : null;
 };
 
-const Controls = () => {
-  const { changePage, currentPage } = useContext(WizzardContext);
+const Controls = (props) => {
+  const { changePage, currentPage, pageIndexes } = useContext(WizzardContext);
 
   return (
-    <div className={cx(styles.ButtonsBox)}>
-      <button className={cx("button", styles.Button)} onClick={() => changePage(currentPage - 1)}>
+    <div {...props} className={cx(styles.ButtonsBox)}>
+      <button
+        disabled={currentPage === 1 && "disabled"}
+        className={cx("button", styles.Button)}
+        onClick={() => changePage(currentPage - 1)}
+      >
         Previous
       </button>
-      <button className={cx("button", styles.Button)} onClick={() => changePage(currentPage + 1)}>
+      <button
+        disabled={currentPage === 3 && "disabled"}
+        className={cx("button", styles.Button)}
+        onClick={() => changePage(currentPage + 1)}
+      >
         Next
       </button>
-      <button className={cx("button", styles.Button)}>Submit</button>
+      {currentPage === 3 && <button className={cx("button", styles.Button)}>Submit</button>}
     </div>
   );
 };
@@ -27,10 +39,21 @@ const Controls = () => {
 const WizzardContext = React.createContext({
   currentPage: 1,
   changePage: () => {},
+  pageIndexes: [],
+  updatePageIndexes: () => {},
 });
 
 const Wizzard = ({ children }) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [pageIndexes, setPageIndexes] = useState([]);
+
+  const updatePageIndexes = (pageIndex) => {
+    if (pageIndexes.includes(pageIndex)) {
+      return;
+    }
+
+    setPageIndexes([...pageIndexes, pageIndex]);
+  };
 
   const changePage = (newPageIndex) => {
     setCurrentPage(newPageIndex);
@@ -41,6 +64,8 @@ const Wizzard = ({ children }) => {
       value={{
         currentPage,
         changePage,
+        pageIndexes,
+        updatePageIndexes,
       }}
     >
       {children}
--- src/views/Users.js
+++ src/views/Users.js
@@ -4,38 +4,47 @@ import cx from "classnames";
 import styles from "./Users.module.scss";
 
 const Users = () => (
-  <div className={cx(styles.Container)}>
-    <MultiStep.Wizzard>
-      <div className="box">
-        <MultiStep.Page pageIndex={1}>
-          <div className={cx(styles.Wrapper)}>
-            <div className="field">
-              <div className={cx("control", styles.Control)}>
-                <label htmlFor="name" className="label">
-                  What is your name
-                </label>
-                <input name="name" className="input is-primary" type="text" placeholder="What is your name" />
-              </div>
+  <MultiStep.Wizzard>
+    <div className="box">
+      <MultiStep.Page pageIndex={1}>
+        <div className={cx(styles.Wrapper)}>
+          <div className="field">
+            <div className={cx("control", styles.Control)}>
+              <label htmlFor="name" className="label">
+                What is your name
+              </label>
+              <input name="name" className="input is-primary" type="text" placeholder="What is your name" />
             </div>
           </div>
-        </MultiStep.Page>
-        <MultiStep.Page pageIndex={2}>
-          <div className={cx(styles.Wrapper)}>
-            <div className="field">
-              <div className={cx("control", styles.Control)}>
-                <label htmlFor="name" className="label">
-                  What is your surname
-                </label>
-                <input name="name" className="input is-primary" type="text" placeholder="What is your surname" />
-              </div>
+        </div>
+      </MultiStep.Page>
+      <MultiStep.Page pageIndex={2}>
+        <div className={cx(styles.Wrapper)}>
+          <div className="field">
+            <div className={cx("control", styles.Control)}>
+              <label htmlFor="name" className="label">
+                What is your surname
+              </label>
+              <input name="name" className="input is-primary" type="text" placeholder="What is your surname" />
             </div>
           </div>
-        </MultiStep.Page>
-        <MultiStep.Page pageIndex={3}>Page 3</MultiStep.Page>
-        <MultiStep.Controls />
-      </div>
-    </MultiStep.Wizzard>
-  </div>
+        </div>
+      </MultiStep.Page>
+      <MultiStep.Page pageIndex={3}>
+        <div className={cx(styles.Wrapper)}>
+          <div className="field">
+            <div className={cx("control", styles.Control)}>
+              <label htmlFor="name" className="label">
+                What is your email address
+              </label>
+              <input name="name" className="input is-primary" type="text" placeholder="What is your email address" />
+            </div>
+          </div>
+        </div>
+      </MultiStep.Page>
+      <MultiStep.Controls />
+    </div>
+  </MultiStep.Wizzard>
 );
 
 export default Users;
